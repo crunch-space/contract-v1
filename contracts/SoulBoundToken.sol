e80b1b8ddb44620d06232124e3f7e396495367ae
Eric <yanghao@10d.xin>
2024-10-17 11:39:06 +0800

    feat: Add price functionality to minting process
    This commit adds functionality to set a price for minting a SoulBoundToken and ensures that the sender has sufficient funds to cover the price. If the sender sends more than the required price, the excess amount is refunded. Additionally, the contract owner can now set the price and withdraw the contract's balance.

--- contracts/SoulBoundToken.sol
+++ contracts/SoulBoundToken.sol
@@ -9,18 +9,22 @@ contract SoulBoundToken is KIP17, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    uint256 internal price;
 
     constructor() KIP17("SoulBoundToken", "SBT") {}
 
-    //TODO sign the minting transaction
-    function safeMint(address to) public {
+    function mint(address to) public payable {
         require(
             balanceOf(to) == 0,
             "This address already has a SoulBoundToken"
         );
+        require(msg.value >= price, "Insufficient funds");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        if (msg.value > price) {
+            payable(msg.sender).transfer(msg.value - price);
+        }
     }
 
     function _beforeTokenTransfer(
@@ -34,7 +38,16 @@ contract SoulBoundToken is KIP17, Ownable {
         );
     }
 
-    function _burn(uint256 tokenId) internal override(KIP17) {
-        super._burn(tokenId);
+    function setPrice(uint256 _price) public onlyOwner {
+        require(_price > 0, "the price must be more than 0!");
+        price = _price;
+    }
+
+    function getPrice() public view returns (uint256) {
+        return price;
+    }
+
+    function withdraw() public payable onlyOwner {
+        payable(owner()).transfer(address(this).balance);
     }
 }
